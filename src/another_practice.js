ea4cffd356c4eca4f03eb6aeff5909a1b3ff8fca
Annie1790 <budayaniko@outlook.com>
2023-03-06 13:51:58 +0000

    .

--- src/another_practice.js
+++ src/another_practice.js
@@ -5,7 +5,7 @@ const items = [
     {productName:"Gilvesy Bohem Cuvee", vintage: "2020", price:"£29"},
     {productName:"Domaine De Trevaillon", vintage: "2005", price:"£300"},
     {productName:"Picpoul De Pinet", vintage: "2022", price:"£26"},
-    {productName:"Felton Road Bannockburn", vintage: "2018", price:"£75"},
+    {productName:"Felton Road", vintage: "2018", price:"£75"},
     {productName:"Dingic Plavac Mali", vintage: "2017", price:"£89"},
     {productName:"Chablis 1Er Cru", vintage: "2007", price:"£400"},
     {productName:"Riesling Trocken", vintage: "2021", price:"£48"}
@@ -16,7 +16,14 @@ let Board = () => {
     
 
     let clickEvent = () => {
-        setIndex((index+1)%items.length);
+        // setIndex((index+1)%items.length);
+
+        if (index+1 >= items.length) {
+            setIndex(0)
+        } else {
+            setIndex(index+1)
+        }
+        
 
         //% means that whenever the index reaches at 8, 8%8 equals to zero therefore the index start from 0 again
     }
